9be739f0684b8017c6984d7f0b1c3ed2ef63fdc0
Rob Steller <robbiestells@hotmail.com>
2016-11-04 11:58:10 -0400

    Track sale button on main screen works

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -12,7 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
--- app/src/main/java/data/ProductCursorAdapter.java
+++ app/src/main/java/data/ProductCursorAdapter.java
@@ -1,16 +1,23 @@
 package data;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android.inventory.MainActivity;
 import com.example.android.inventory.R;
 
 /**
@@ -19,7 +26,7 @@ import com.example.android.inventory.R;
 
 public class ProductCursorAdapter extends CursorAdapter {
 
-    public ProductCursorAdapter(Context context, Cursor c){
+    public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
 
@@ -31,9 +38,11 @@ public class ProductCursorAdapter extends CursorAdapter {
 
     //binds data to list item
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        TextView tvName = (TextView) view.findViewById(R.id.product_name);
-        TextView tvQuantity = (TextView) view.findViewById(R.id.product_quantity);
+    public void bindView(final View view, final Context context, final Cursor cursor) {
+        final int itemId = cursor.getInt(cursor.getColumnIndex(ProductContract.ProductEntry._ID));
+
+        final TextView tvName = (TextView) view.findViewById(R.id.product_name);
+        final TextView tvQuantity = (TextView) view.findViewById(R.id.product_quantity);
         WebView wbImage = (WebView) view.findViewById(R.id.product_image);
 
         int nameColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
@@ -41,7 +50,7 @@ public class ProductCursorAdapter extends CursorAdapter {
         int imageColumnIndext = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_IMAGE);
 
         String name = cursor.getString(nameColumnIndex);
-        int quantity = cursor.getInt(quantityColumnIndex);
+        final int quantity = cursor.getInt(quantityColumnIndex);
         String image = cursor.getString(imageColumnIndext);
 
         tvName.setText(name);
@@ -51,5 +60,22 @@ public class ProductCursorAdapter extends CursorAdapter {
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
         wbImage.loadUrl(image);
+
+        Button saleButton = (Button) view.findViewById(R.id.decrease_button);
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+
+                if (quantity > 0){
+                    int newQuantity = quantity - 1;
+                    values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, newQuantity);
+                    Uri uri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, itemId);
+                    context.getContentResolver().update(uri,values, null, null);
+                    //tvQuantity.setText(String.valueOf(newQuantity));
+                }
+                context.getContentResolver().notifyChange(ProductContract.ProductEntry.CONTENT_URI, null);
+            }
+        });
     }
 }
